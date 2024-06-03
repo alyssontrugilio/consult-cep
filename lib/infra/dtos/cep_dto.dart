0dbb65bdce2baf6a085f22a8f7c8cb5045af4d48
AlyssonTrugilio <alyson.silvatrugilio121@gmail.com>
2024-06-03 08:52:48 -0300

    refactor: ajustado dto implementando as tipagem

--- lib/infra/dtos/cep_dto.dart
+++ lib/infra/dtos/cep_dto.dart
@@ -1,24 +1,18 @@
-import 'dart:convert';
-
 import '../../domain/domain.dart';
 
 extension CepDto on CepEntity {
   static CepEntity fromMap(Map<String, dynamic> map) {
     return CepEntity(
-      cep: map['cep'],
-      logradouro: map['logradouro'],
-      complemento: map['complemento'],
-      localidade: map['localidade'],
-      bairro: map['bairro'],
-      uf: map['uf'],
-      ibge: map['ibge'],
-      gia: map['gia'],
-      ddd: map['ddd'],
-      siafi: map['siafi'],
+      cep: map['cep'] as String,
+      logradouro: map['logradouro'] as String,
+      complemento: map['complemento'] as String,
+      localidade: map['localidade'] as String,
+      bairro: map['bairro'] as String,
+      uf: map['uf'] as String,
+      ibge: map['ibge'] as String,
+      gia: map['gia'] as String,
+      ddd: map['ddd'] as String,
+      siafi: map['siafi'] as String,
     );
-  }
-
-  static CepEntity fromJson(String json) {
-    return fromMap(jsonDecode(json));
   }
 }
